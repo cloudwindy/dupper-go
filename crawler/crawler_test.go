0f1f09d634f2a27e38e92eae0581249af001a5fc
cloudwindy <49033775+cloudwindy@users.noreply.github.com>
2019-12-14 22:50:53 +0800

    Add files via upload

---
+++ crawler/crawler_test.go
@@ -0,0 +1,56 @@
+package crawler
+
+import (
+	"fmt"
+	"testing"
+)
+
+// TEST PASSED
+func testFetchTags(t *testing.T) {
+	tags := FetchTags()
+	if len(tags) <= 0 {
+		t.Errorf("No tag found")
+		return
+	}
+	fmt.Println("Tags count:", len(tags))
+	return
+}
+
+// TEST PASSED
+func testNewComic(t *testing.T) {
+	comic := NewComic(274506)
+	comic.Info()
+	return
+}
+
+// TEST PASSED
+func testDownload(t *testing.T) {
+	comic := NewComic(274506)
+	comic.Download("./save")
+}
+
+// TEST PASSED
+func TestFetchPage(t *testing.T) {
+	comics := FetchPage(1)
+	for _, comic := range comics {
+		comic.Info()
+	}
+}
+
+func (comic Comic) Info() {
+	fmt.Println("本子 ID: ", comic.ID)
+	fmt.Println("英文名称: ", comic.EnglishName)
+	fmt.Println("原文名称: ", comic.OriginalName)
+	fmt.Println("系列: ", comic.Parodies)
+	fmt.Println("标签: ", comic.Tags)
+	fmt.Println("作者: ", comic.Artists)
+	fmt.Println("团体: ", comic.Groups)
+	fmt.Println("已翻译: ", comic.Translated)
+	fmt.Println("语言: ", comic.Language)
+	fmt.Println("分类: ", comic.Categories)
+	fmt.Println("下载 ID: ", comic.DownloadID)
+	fmt.Println("下载页数:", comic.DownloadNum)
+	fmt.Println("下载语言代码:", comic.DownloadLan)
+	fmt.Println("下载文件后缀:", comic.DownloadExt)
+	return
+}
---
+++ crawler/go.mod
@@ -0,0 +1,12 @@
+module github.com/cloudwindy/dupper-go/crawler
+
+go 1.12
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/andybalholm/cascadia v1.1.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
+)
